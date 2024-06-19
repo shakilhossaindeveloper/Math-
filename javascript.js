89db59c3dcdd308388db73b7c0aa8152542d09de
shakilhossaindeveloper <shakilhossain.developer@gmail.com>
2024-06-19 22:05:30 +0600

    cal

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let sum =  parseFloat(prompt("Enter  1st Number= "));
+let ssum =parseFloat(prompt("Enter  2nd Number= "));
+
+const answer = sum + ssum;
+document.write("Correct Answer = "+answer);
